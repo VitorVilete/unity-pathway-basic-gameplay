e5228dc5c26fc03f1e8e70fa3b9934bfa9a234a9
Vitor Vilete <vitorgarcesvilete@gmail.com>
2023-06-30 14:38:57 -0300

    Initial commit. This is a finished project

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnDelay = 1;
+    private float lastDogSpawnedTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - lastDogSpawnedTime >= spawnDelay)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastDogSpawnedTime = Time.time;
+        }
+    }
+}
---
+++ Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Destroys the gameObject that collided with our gameObject
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+}
